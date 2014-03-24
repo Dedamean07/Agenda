bdc9b6c371c4dc903e99078ade5d772c56ae626c
Alumno <alumno@localhost.localdomain>
2014-03-24 08:59:28 -0600

    Agenda

---
+++ src/Agendaa.java
@@ -0,0 +1,35 @@
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+
+
+/**
+ *
+ * @author Denis
+ */
+public class Agendaa {
+   
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+       
+        
+        int arre[]= {98949596,93929596,94959692,96939291,99949596};
+          
+        System.out.println("Agenda");
+        for(int i=0;i<arre.length;i++){
+        
+        System.out.println("El numero "+(i+1)+" es: "+arre[i]);
+    }
+        
+        
+        
+    }
+}
